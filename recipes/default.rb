8adac85a660095ae8940714d2077259e98c4bcaf
Joe Gardiner <joe@grdnr.io>
2016-12-12 12:23:07 +0000

    Helper cookbook commenting

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+# This is a helper cookbook so nothing executes in the default recipe file.
+# Please refer to the accompanying README for more information.
+#
