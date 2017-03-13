afe258225d07a0a9be57de38ca56662bc784ed5a
Christoph Hartmann <chris@lollyrock.com>
2017-03-14 00:09:37 +0100

    use policy_template attribute to avoid conflict with chef namespace
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
 name 'windows-security-policy'
 maintainer 'Joe Gardiner'
 maintainer_email 'joe@grdnr.io'
-license 'all_rights'
+license 'Apache 2.0'
 description 'Configures Windows security policy'
 long_description 'A helper cookbook that allows you to use attributes and custom resources to manage your local security policy on Windows.'
 version '0.3.3'
 
-# issues_url 'https://github.com/<insert_org_here>/windows-security-policy/issues' if respond_to?(:issues_url)
-
-# source_url 'https://github.com/<insert_org_here>/windows-security-policy' if respond_to?(:source_url)
+issues_url 'https://github.com/grdnrio/windows-security-policy/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/grdnrio/windows-security-policy' if respond_to?(:source_url)
--- resources/security_policy.rb
+++ resources/security_policy.rb
@@ -1,18 +1,18 @@
 resource_name :security_policy
 
-property :template, String, required: true, default: 'C:\Windows\security\templates\chefNewPolicy.inf'
-property :database, String, required: true, default: 'C:\Windows\security\database\chef.sdb'
+property :policy_template, String, required: false, default: 'C:\Windows\security\templates\chefNewPolicy.inf'
+property :database, String, required: false, default: 'C:\Windows\security\database\chef.sdb'
 property :log_location, String, default: 'C:\Windows\security\logs\chef-secedit.log'
 
 action :configure do
   if node['platform'] == 'windows'
-    template '#{template}' do
+    template "#{policy_template}" do
       source 'policy.inf.erb'
       action :create
     end
 
     execute 'Configure security database' do
-      command "Secedit /configure /db #{database} /cfg #{template} /log #{log_location}"
+      command "Secedit /configure /db #{database} /cfg #{policy_template} /log #{log_location}"
       live_stream true
       action :run
     end
@@ -24,7 +24,7 @@ end
 action :export do
   if node['platform'] == 'windows'
     execute 'Export security database to inf file' do
-      command "Secedit /export /db #{database} /cfg #{template} /log #{log_location}"
+      command "Secedit /export /db #{database} /cfg #{policy_template} /log #{log_location}"
       live_stream true
       action :run
     end
@@ -41,7 +41,7 @@ action :import do
     end
 
     execute 'Import and create security database' do
-      command "Secedit /import /db #{database} /cfg #{template} /log #{log_location} /overwrite"
+      command "Secedit /import /db #{database} /cfg #{policy_template} /log #{log_location} /overwrite"
       live_stream true
       action :run
     end
