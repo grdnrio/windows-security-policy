28cd9a7b84c8df4a4a7f76de401a6aa42acec71e
Joe Gardiner <joe@grdnr.io>
2017-03-21 09:24:30 +0000

    Added supports windows

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joe@grdnr.io'
 license 'Apache 2.0'
 description 'Configures Windows security policy'
 long_description 'A helper cookbook that allows you to use attributes and custom resources to manage your local security policy on Windows.'
-version '0.3.4'
-
+version '0.3.5'
+supports 'windows'
 issues_url 'https://github.com/grdnrio/windows-security-policy/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/grdnrio/windows-security-policy' if respond_to?(:source_url)
