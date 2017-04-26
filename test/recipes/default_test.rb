c66c0db6915ed311d1f34825265570d14a93414b
Joe Gardiner <joe@grdnr.io>
2017-04-26 16:25:37 +0100

    Basic appveyor template test

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -5,14 +5,6 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
-end
-
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe file('C:\Windows\security\templates\mySecurityPolicy.inf') do
+  it { should exist }
 end
