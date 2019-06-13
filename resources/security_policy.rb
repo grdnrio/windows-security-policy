de76f43c47c2d9c4133ec9313d2ee41a2fccedeb
Andrew Bounds <asbounds@gmail.com>
2019-06-13 09:13:33 -0400

    add rollback resource

--- resources/security_policy.rb
+++ resources/security_policy.rb
@@ -4,6 +4,7 @@ default_action :configure
 
 property :policy_template, String, required: false, default: 'C:\Windows\security\templates\chefNewPolicy.inf'
 property :database, String, required: false, default: 'C:\Windows\security\database\chef.sdb'
+property :rollback_template, String, required: false, default: 'C:\Windows\security\templates\chefNewPolicy.rbk.inf'
 property :log_location, String, default: 'C:\Windows\security\logs\chef-secedit.log'
 
 action :configure do
@@ -45,6 +46,23 @@ action :import do
 
     execute 'Import and create security database' do
       command "Secedit /import /db #{new_resource.database} /cfg #{new_resource.policy_template} /log #{new_resource.log_location} /overwrite"
+      live_stream true
+      action :run
+    end
+  else
+    Chef::Log.info "#{@current_resource} is only for a Windows platform"
+  end
+end
+
+action :rollback do
+  if node['platform'] == 'windows'
+    template new_resource.policy_template do
+      source 'policy.inf.erb'
+      action :create
+    end
+
+    execute 'Import and create security database' do
+      command "Secedit /generaterollback /cfg #{new_resource.policy_template} /rbk #{new_resource.rollback_template} /log #{new_resource.log_location} /overwrite"
       live_stream true
       action :run
     end
